a2d8db54360a84fd513b722a63098a1c71d596b7
nicolastaen <nic.tapiae@duocuc.cl>
2026-03-25 13:32:06 -0300

    arreglo?

--- src/main/java/biblioteca_libros/model/Libro.java
+++ src/main/java/biblioteca_libros/model/Libro.java
@@ -1,5 +1,18 @@
 package biblioteca_libros.model;
 
-public class libro {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libro {
+
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;
 }
--- src/main/java/biblioteca_libros/repository/LibroRepostory.java
+++ src/main/java/biblioteca_libros/repository/LibroRepostory.java
@@ -1,5 +1,21 @@
 package biblioteca_libros.repository;
 
-public class libroRepostory {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import biblioteca_libros.model.Libro;
+
+@Repository
+public class LibroRepostory {
+
+    //Arreglo para guardar los libeos
+    private List<Libro> listarLibros = new ArrayList<>();
+
+    // Metodo para retornar los libros
+    public List<Libro> obtenerLibros() {
+        return listarLibros;
+    }
 
 }
